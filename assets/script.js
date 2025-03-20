96d442a2053cd34b14539cb20ce514c64ab3a322
Nicolas Rodriguez <niicorodriguez@gmail.com>
2025-03-20 14:09:11 -0500

    Actualización de la página 'Nosotros' con mejoras en HTML, CSS y JS

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
